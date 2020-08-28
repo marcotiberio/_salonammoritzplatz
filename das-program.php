515979bca08f828ac8d869e5960f13cbab7a788a
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-28 13:40:20 +0200

    test das program template

---
+++ das-program.php
@@ -0,0 +1,59 @@
+<?php 
+	/* Template Name: Latest Posts Articles */ 
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary--page-main" class="content-area--page-main">
+		<main id="main--page-main" class="site-main--page-main">
+			
+			<header class="entry-header">
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+			</header>
+
+		<?php 
+			$args = array(
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'category_name' => 'event',
+				'posts_per_page' => 3,
+			);
+			$arr_posts = new WP_Query( $args );
+			 
+			if ( $arr_posts->have_posts() ) :
+			 
+				while ( $arr_posts->have_posts() ) :
+					$arr_posts->the_post();
+					?>
+					<article class="latestpost--custom" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<div>
+                            <?php
+                            if ( has_post_thumbnail() ) :
+                                the_post_thumbnail( 'full' );
+                            endif;
+                            ?>
+						</div>
+						<div>
+						<div class="event-title">
+                            <p>
+                            10.02.2020, 14:00<br>
+                            12,00€<br>
+                            Ticker
+                            <p>
+						</div>
+						<div class="event-intro">
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantiumdoloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantiumdoloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+						</div>
+						</div>
+					</article>
+					<?php
+				endwhile;
+			endif; ?>
+		
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
