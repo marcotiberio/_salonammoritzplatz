b4a4dd73aa942e24e6bc19fce7c1e51bf4f5293e
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-31 16:01:55 +0200

    test script position nav

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-$(document).ready(function(){   
-    $(window).scroll(function() {
-        if ($(this).scrollTop()>0)
-        {
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
             $('#stickyTickertape').fadeIn();
-        }
-        else
-        {
-        $('#stickyTickertape').fadeOut('100');
+        } else {
+            $('#stickyTickertape').fadeOut('100');
         }
     });
 });
 
-$(document).ready(function(){   
-    $(window).scroll(function() {
-        if ($(this).scrollTop()>0)
-        {
-            $('.tickerTape').fadeOut();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.tickerTape').css("top", "5vh");
+        } else {
+            $('.tickerTape').css("top", "20vh");
         }
-        else
-        {
-        $('.tickerTape').fadeIn('100');
+    });
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.#primary-menu').css();
+        } else {
+            $('.tickerTape').fadeIn('100');
         }
     });
 });
@@ -30,10 +34,9 @@ $(document).ready(function(){
 //         $("#menuOverlay").slideToggle("fast");
 //     });
 // });
-  
+
 // $(document).ready(function(){
 //     $("#menu-toggleMobile").click(function(){
 //         $("#menuOverlay").slideToggle("fast");
 //     });
 // });
-
