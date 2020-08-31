31675f1e68ef6acde0df8bfe53421c1f1ce133f5
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-31 15:28:43 +0200

    test

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         }
         else
         {
-        $('#stickyPrimaryMenu').fadeOut(200);
+        $('#stickyPrimaryMenu').fadeOut('fast');
         }
     });
 });
