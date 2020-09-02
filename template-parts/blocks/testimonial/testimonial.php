3bc081ccd7f03a259cf0ff4f3bbf4fc0c7bd2d1f
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-02 15:42:27 +0200

    title link field

--- template-parts/blocks/testimonial/testimonial.php
+++ template-parts/blocks/testimonial/testimonial.php
@@ -40,6 +40,16 @@ $title = get_field('title') ?: 'Testimonial title';
     </div>
     <div class="testimonial-info">
         <p><?php echo $date; ?></p>
-        <h2><?php echo $title; ?></h2>
+        <h2>
+        <?php 
+            $link = get_field('title');
+            if( $link ): 
+                $link_url = $link['url'];
+                $link_title = $link['title'];
+                $link_target = $link['target'] ? $link['target'] : '_self';
+                ?>
+                <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+            <?php endif; ?>
+        </h2>
     </div>
 </div>
