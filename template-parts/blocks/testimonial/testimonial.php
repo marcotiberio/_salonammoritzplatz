499e777de630bf36839b5aced12ad7c4a7fed51c
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-02 15:34:24 +0200

    fix names class

--- template-parts/blocks/testimonial/testimonial.php
+++ template-parts/blocks/testimonial/testimonial.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Event Block Template.
+ * Testimonial Block Template.
  *
  * @param   array $block The block settings and attributes.
  * @param   string $content The block inner HTML (empty).
@@ -16,7 +16,7 @@ if( !empty($block['anchor']) ) {
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
-$className = 'event';
+$className = 'testimonial';
 if( !empty($block['className']) ) {
     $className .= ' ' . $block['className'];
 }
@@ -32,7 +32,7 @@ $title = get_field('title') ?: 'Testimonial title';
 ?>
 <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
     <div class="testimonial-cover">
-            <?php echo wp_get_attachment_image( $image, 'full' ); ?>
+        <?php echo wp_get_attachment_image( $image, 'full' ); ?>
     </div>
     <div class="testimonial-info">
         <p><?php echo $date; ?></p>
