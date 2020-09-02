34c465cc9eed242fe9713422c7e68fe1dcdd195e
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-02 14:13:26 +0200

    test modal

--- template-parts/blocks/event/event.php
+++ template-parts/blocks/event/event.php
@@ -52,3 +52,42 @@ $referrer = get_field('referrer') ?: 'Link of the event';
         </div>
     </div>
 </div>
+
+<!-- Booking Modal -->
+<div id="bookingModal" class="modal">
+
+  <!-- Booking Modal - Content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById("bookingModal");
+
+// Get the button that opens the modal
+var btn = document.getElementByClassName("booking");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
