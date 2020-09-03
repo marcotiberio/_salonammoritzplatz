2637a998c1b635a12ca9eeaf225c8d596f9a2c5b
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-03 11:06:47 +0200

    readded slick

---
+++ js/slick-init.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+	$('.team-items').slick({ //add in your correct containing element
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 2000,
+	});
+});
