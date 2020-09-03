fa2a6fee174be61a5f58f83fdbd23a744e2f0c34
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-03 17:20:36 +0200

    test

--- das-program.php
+++ das-program.php
@@ -73,6 +73,48 @@
 							<div class="details"><?php the_field('description'); ?></div>
 						</div>
 					</article>
+
+					<!-- Booking Modal -->
+					<div id="bookingModal-<?php the_ID(); ?>" class="modal">
+
+					<!-- Booking Modal - Content -->
+					<div class="modal-content">
+						<span class="close">&times;</span>
+						<p><?php the_field('type'); ?></p>
+						<h2><?php the_field('title'); ?></h2>
+						<p><?php the_field('when'); ?></p>
+						<div class="form"><?php the_field('form'); ?></div>
+					</div>
+
+					</div>
+
+					<script>
+					// Get the modal
+					var modal = document.getElementById("bookingModal-<?php the_ID(); ?>");
+
+					// Get the button that opens the modal
+					var btn = document.getElementById("booking-<?php the_ID(); ?>");
+
+					// Get the <span> element that closes the modal
+					var span = document.getElementsByClassName("close")[0];
+
+					// When the user clicks the button, open the modal 
+					btn.onclick = function () {
+						modal.style.display = "block";
+					}
+
+					// When the user clicks on <span> (x), close the modal
+					span.onclick = function () {
+						modal.style.display = "none";
+					}
+
+					// When the user clicks anywhere outside of the modal, close it
+					window.onclick = function (event) {
+						if (event.target == modal) {
+							modal.style.display = "none";
+						}
+					}
+					</script>
 					<?php
 				endwhile;
 			endif; ?>
@@ -80,48 +122,6 @@
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-	<!-- Booking Modal -->
-	<div id="bookingModal-<?php the_ID(); ?>" class="modal">
-
-		<!-- Booking Modal - Content -->
-		<div class="modal-content">
-			<span class="close">&times;</span>
-			<p><?php the_field('type'); ?></p>
-			<h2><?php the_field('title'); ?></h2>
-			<p><?php the_field('when'); ?></p>
-			<div class="form"><?php the_field('form'); ?></div>
-		</div>
-
-	</div>
-
-	<script>
-		// Get the modal
-		var modal = document.getElementById("bookingModal-<?php the_ID(); ?>");
-
-		// Get the button that opens the modal
-		var btn = document.getElementById("booking-<?php the_ID(); ?>");
-
-		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
-
-		// When the user clicks the button, open the modal 
-		btn.onclick = function () {
-			modal.style.display = "block";
-		}
-
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = function () {
-			modal.style.display = "none";
-		}
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function (event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
-			}
-		}
-	</script>
 
 <?php
 get_sidebar();
