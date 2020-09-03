c3ca6ae92f466d64516a3ad2044e5376a31bd028
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-03 16:57:46 +0200

    event ticker tape

--- event.php
+++ event.php
@@ -11,61 +11,94 @@
 get_header();
 ?>
 
-<main id="primary" class="site-main">
+	<main id="primary" class="site-main">
 
-	<?php
-		while ( have_posts() ) :
-			the_post();
+	<div class="ticker-tape-small">
+			<div class="inside">
+				<svg class="first" width="2054" height="28" viewBox="0 0 2054 28">
+					<text id="Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_" data-name="Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        " transform="translate(1027 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1026.792" y="0" xml:space="preserve">AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        </tspan></text>
+				</svg>
+				<svg width="2054" height="28" viewBox="0 0 2054 28">
+					<text id="Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_" data-name="Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        " transform="translate(1027 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1026.792" y="0" xml:space="preserve">AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        </tspan></text>
+				</svg>
+				<svg width="2054" height="28" viewBox="0 0 2054 28">
+					<text id="Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_" data-name="Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        " transform="translate(1027 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1026.792" y="0" xml:space="preserve">AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        </tspan></text>
+				</svg>
+				<svg width="2054" height="28" viewBox="0 0 2054 28">
+					<text id="Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_" data-name="Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        " transform="translate(1027 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1026.792" y="0" xml:space="preserve">AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        </tspan></text>
+				</svg>
+				<svg width="2054" height="28" viewBox="0 0 2054 28">
+					<text id="Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_" data-name="Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        " transform="translate(1027 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1026.792" y="0" xml:space="preserve">AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        </tspan></text>
+				</svg>
+				<svg width="2054" height="28" viewBox="0 0 2054 28">
+					<text id="Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_" data-name="Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        " transform="translate(1027 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1026.792" y="0" xml:space="preserve">AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        </tspan></text>
+				</svg>
+				<!-- repeated images below to keep up with animation -->
+				<svg width="2054" height="28" viewBox="0 0 2054 28">
+					<text id="Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_" data-name="Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        " transform="translate(1027 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1026.792" y="0" xml:space="preserve">AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        </tspan></text>
+				</svg>
+				<svg width="2054" height="28" viewBox="0 0 2054 28">
+					<text id="Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_" data-name="Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        " transform="translate(1027 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1026.792" y="0" xml:space="preserve">AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        </tspan></text>
+				</svg>
+				<svg width="2054" height="28" viewBox="0 0 2054 28">
+					<text id="Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_Aktuelles_" data-name="Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        Aktuelles        " transform="translate(1027 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1026.792" y="0" xml:space="preserve">AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        AKTUELLES        </tspan></text>
+				</svg>
+			</div>
+		</div>
 
-			get_template_part( 'template-parts/content-event', get_post_type() );
+		<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content-event', get_post_type() );
 
 
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
 
-</main><!-- #main -->
+	</main><!-- #main -->
 
-<!-- Booking Modal -->
-<div id="bookingModal" class="modal">
+	<!-- Booking Modal -->
+	<div id="bookingModal" class="modal">
 
-	<!-- Booking Modal - Content -->
-	<div class="modal-content">
-		<span class="close">&times;</span>
-		<p><?php the_field('type'); ?></p>
-		<h2><?php the_field('title'); ?></h2>
-		<p><?php the_field('when'); ?></p>
-		<div class="form"><?php the_field('form'); ?></div>
+		<!-- Booking Modal - Content -->
+		<div class="modal-content">
+			<span class="close">&times;</span>
+			<p><?php the_field('type'); ?></p>
+			<h2><?php the_field('title'); ?></h2>
+			<p><?php the_field('when'); ?></p>
+			<div class="form"><?php the_field('form'); ?></div>
+		</div>
+
 	</div>
 
-</div>
+	<script>
+		// Get the modal
+		var modal = document.getElementById("bookingModal");
 
-<script>
-	// Get the modal
-	var modal = document.getElementById("bookingModal");
+		// Get the button that opens the modal
+		var btn = document.getElementById("booking");
 
-	// Get the button that opens the modal
-	var btn = document.getElementById("booking");
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
 
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
+		// When the user clicks the button, open the modal 
+		btn.onclick = function () {
+			modal.style.display = "block";
+		}
 
-	// When the user clicks the button, open the modal 
-	btn.onclick = function () {
-		modal.style.display = "block";
-	}
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function () {
-		modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function (event) {
-		if (event.target == modal) {
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function () {
 			modal.style.display = "none";
 		}
-	}
-</script>
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function (event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
+	</script>
 
 <?php
 get_sidebar();
