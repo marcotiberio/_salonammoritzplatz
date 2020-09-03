de9572427a66ace9ed964420e4888116010336f9
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-03 10:56:46 +0200

    removed every slider

--- der-salon.php
+++ der-salon.php
@@ -150,34 +150,7 @@ get_header();
 			</div>
 		</div>
 
-		<?php  //add me in
-
-			//Fields
-
-			//slider_portfolio = Gallery Field
-
-			$images = get_field('slider_portfolio');
-
-			if( $images ): ?>
-			<div class="slider-for">
-					
-						<?php foreach( $images as $image ): ?>
-							<div class="slick-container">
-								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-								
-							</div>
-						<?php endforeach; ?>
-				</div>
-			<div class="slider-nav">
-					
-						<?php foreach( $images as $image ): ?>
-							<div>
-								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-								
-							</div>
-						<?php endforeach; ?>
-				</div>
-			<?php endif; ?>
+		<!--Slider-->
 
 		<div class="ticker-tape-small">
 			<div class="inside">
