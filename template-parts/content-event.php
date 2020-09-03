3fc7d47d767006d8bfa8afc36165d1cbedb05996
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-03 16:19:37 +0200

    test event template

--- template-parts/content-event.php
+++ template-parts/content-event.php
@@ -18,17 +18,13 @@
 		<div class="event-header">
 			<p><?php the_field('type'); ?></p>
 			<h2><?php the_field('title'); ?></h2>
+			<p><?php the_field('when'); ?></p>
+			<p><?php the_field('price'); ?></p>
+			<p id="booking"><?php the_field('booking'); ?></p>
+			<p><?php the_field('referrer'); ?></p>
 		</div>
 		<div class="event-info">
-			<div class="left">
-				<p><?php the_field('when'); ?></p>
-				<p><?php the_field('price'); ?></p>
-				<p id="booking"><?php the_field('booking'); ?></p>
-				<p><?php the_field('referrer'); ?></p>
-			</div>
-			<div class="right">
-				<div class="details"><?php the_field('description'); ?></div>
-			</div>
+			<div class="details"><?php the_field('description'); ?></div>
 		</div>
 		
 	</div><!-- .entry-content -->
