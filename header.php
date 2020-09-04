f06cb1a1791a19e28cb29b71ae2885d87d57bc2f
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-04 14:05:29 +0200

    test

--- header.php
+++ header.php
@@ -45,61 +45,62 @@
 				<p class="site-description"><?php echo $salonammoritzplatz_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 			
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 				<div id="tickerTape">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 					<div class="text">
 						<svg class="first" width="3065" height="85" viewBox="0 0 3065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
 						<!-- repeated images below to keep up with animation -->
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
-						<svg width="3065" height="85" viewBox="0 0 3065 85">
+						<svg width="3065" height="85" viewBox="0 0 2065 85">
 							<text id="SalonamMoritzplatz" data-name="Salon AM Moritzplatz          Salon AM Moritzplatz          Salon AM Moritzplatz          " transform="translate(0 46)" fill="#3e3e3e" font-size="40" font-family="sam-arial-black" font-weight="800" letter-spacing="-0.015em"><tspan x="0" y="0" font-family="sam-arial-black" xml:space="preserve">SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          SALON AM MORITZPLATZ          </tspan></text>
 						</svg>
 					</div>
+				</a>
 				</div>
-			</a>
+			
 
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 				<div id="stickyTickertape">
