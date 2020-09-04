26fa98dd1d3af4433c3be797f9ae37318a1fa23f
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-04 16:26:54 +0200

    archive test

--- das-program.php
+++ das-program.php
@@ -160,12 +160,13 @@
 
 	<section id="archivez">
 		<div id="contentArchivez">
-
-		<?php the_post(); ?>
-		<h2>Recent Articles</h2>
-		<ol><?php wp_get_archives('type=yearly&limit=10'); ?></ol>
-		<h2>Archives by Category:</h2>
-		<ul><?php wp_list_categories(''); ?></ul>
+			<?php the_post(); ?>
+			<h2>Recent Articles</h2>
+			<ol><?php wp_get_archives('type=yearly&limit=10'); ?></ol>
+			<h2>Archives by Category:</h2>
+			<ul><?php wp_list_categories(''); ?></ul>
+		</div>
+	</section>
 <?php
 get_sidebar();
 get_footer();
