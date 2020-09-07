b09173c78ef69e69c6a8c330d433a13d33f0b2b7
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-07 10:46:19 +0200

    blueprint image

---
+++ kontakt.php
@@ -0,0 +1,117 @@
+<?php
+/* Template Name: Kontakt */ 
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<div class="ticker-tape-small">
+			<div class="inside">
+				<svg class="first" width="2110" height="28" viewBox="0 0 2110 28">
+					<text id="Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_" data-name="Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          " transform="translate(1055 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1054.453" y="0" xml:space="preserve">KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          </tspan></text>
+				</svg>
+				<svg width="2110" height="28" viewBox="0 0 2110 28">
+					<text id="Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_" data-name="Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          " transform="translate(1055 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1054.453" y="0" xml:space="preserve">KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          </tspan></text>
+				</svg>
+				<svg width="2110" height="28" viewBox="0 0 2110 28">
+					<text id="Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_" data-name="Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          " transform="translate(1055 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1054.453" y="0" xml:space="preserve">KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          </tspan></text>
+				</svg>
+				<svg width="2110" height="28" viewBox="0 0 2110 28">
+					<text id="Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_" data-name="Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          " transform="translate(1055 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1054.453" y="0" xml:space="preserve">KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          </tspan></text>
+				</svg>
+				<svg width="2110" height="28" viewBox="0 0 2110 28">
+					<text id="Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_" data-name="Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          " transform="translate(1055 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1054.453" y="0" xml:space="preserve">KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          </tspan></text>
+				</svg>
+				<svg width="2110" height="28" viewBox="0 0 2110 28">
+					<text id="Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_" data-name="Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          " transform="translate(1055 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1054.453" y="0" xml:space="preserve">KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          </tspan></text>
+				</svg>
+				<!-- repeated images below to keep up with animation -->
+				<svg width="2110" height="28" viewBox="0 0 2110 28">
+					<text id="Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_" data-name="Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          " transform="translate(1055 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1054.453" y="0" xml:space="preserve">KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          </tspan></text>
+				</svg>
+				<svg width="2110" height="28" viewBox="0 0 2110 28">
+					<text id="Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_" data-name="Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          " transform="translate(1055 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1054.453" y="0" xml:space="preserve">KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          </tspan></text>
+				</svg>
+				<svg width="2110" height="28" viewBox="0 0 2110 28">
+					<text id="Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_Kontakt_" data-name="Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          Kontakt          " transform="translate(1055 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1054.453" y="0" xml:space="preserve">KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          KONTAKT          </tspan></text>
+				</svg>
+			</div>
+		</div>
+
+		<!-- Contacts -->
+
+		<div class="contacts">
+
+			<?php if( have_rows('contacts') ): ?>
+				<?php while( have_rows('contacts') ): the_row(); 
+					$role = get_sub_field('role');
+					$name = get_sub_field('name');
+					$email = get_sub_field('email');
+					$phone = get_sub_field('phone');
+					?>
+					<div class="team-repeater">
+						<h2 class="role"><?php the_sub_field('role'); ?></h2>
+						<p class="name"><?php the_sub_field('name'); ?></p>
+						<p class="email"><?php the_sub_field('email'); ?></p>
+						<p class="phone"><?php the_sub_field('phone'); ?></p>
+					</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+
+		</div>
+
+		<!-- Contacts -->
+
+		<div class="ticker-tape-small">
+			<div class="inside">
+				<svg class="first" width="2070" height="28" viewBox="0 0 2070 28">
+					<text id="Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_" data-name="Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          " transform="translate(1035 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1034.473" y="0" xml:space="preserve">ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          </tspan></text>
+				</svg>
+				<svg width="2070" height="28" viewBox="0 0 2070 28">
+					<text id="Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_" data-name="Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          " transform="translate(1035 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1034.473" y="0" xml:space="preserve">ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          </tspan></text>
+				</svg>
+				<svg width="2070" height="28" viewBox="0 0 2070 28">
+					<text id="Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_" data-name="Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          " transform="translate(1035 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1034.473" y="0" xml:space="preserve">ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          </tspan></text>
+				</svg>
+				<svg width="2070" height="28" viewBox="0 0 2070 28">
+					<text id="Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_" data-name="Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          " transform="translate(1035 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1034.473" y="0" xml:space="preserve">ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          </tspan></text>
+				</svg>
+				<svg width="2070" height="28" viewBox="0 0 2070 28">
+					<text id="Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_" data-name="Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          " transform="translate(1035 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1034.473" y="0" xml:space="preserve">ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          </tspan></text>
+				</svg>
+				<svg width="2070" height="28" viewBox="0 0 2070 28">
+					<text id="Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_" data-name="Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          " transform="translate(1035 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1034.473" y="0" xml:space="preserve">ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          </tspan></text>
+				</svg>
+				<!-- repeated images below to keep up with animation -->
+				<svg width="2070" height="28" viewBox="0 0 2070 28">
+					<text id="Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_" data-name="Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          " transform="translate(1035 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1034.473" y="0" xml:space="preserve">ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          </tspan></text>
+				</svg>
+				<svg width="2070" height="28" viewBox="0 0 2070 28">
+					<text id="Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_" data-name="Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          " transform="translate(1035 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1034.473" y="0" xml:space="preserve">ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          </tspan></text>
+				</svg>
+				<svg width="2070" height="28" viewBox="0 0 2070 28">
+					<text id="Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_Anfahrt_" data-name="Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          Anfahrt          " transform="translate(1035 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1034.473" y="0" xml:space="preserve">ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          ANFAHRT          </tspan></text>
+				</svg>
+			</div>
+		</div>
+
+		<!-- Blueprint -->
+
+		<div class="blueprint-image">
+			<?php 
+				$image = get_field('bleprint_image');
+				if( !empty( $image ) ): ?>
+					<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+					<p><?php echo esc_attr($image['caption']); ?></p>
+			<?php endif; ?>
+		</div>
+
+		<!-- Blueprint -->
+
+	</main><!-- #main -->
+
+
+<?php
+get_sidebar();
+get_footer();
