cb73d12f4cab4b5fb44a013651f38d0e4ce7386d
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-07 09:43:52 +0200

    der raum template

---
+++ der-raum.php
@@ -0,0 +1,101 @@
+<?php
+/* Template Name: Der Salon */ 
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<div class="ticker-tape-small">
+			<div class="inside">
+				<svg clas="first" width="2154" height="28" viewBox="0 0 2154 28">
+					<text id="UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_" data-name="UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         " transform="translate(1077 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1076.924" y="0" xml:space="preserve">UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         </tspan></text>
+				</svg>
+				<svg width="2154" height="28" viewBox="0 0 2154 28">
+					<text id="UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_" data-name="UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         " transform="translate(1077 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1076.924" y="0" xml:space="preserve">UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         </tspan></text>
+				</svg>
+				<svg width="2154" height="28" viewBox="0 0 2154 28">
+					<text id="UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_" data-name="UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         " transform="translate(1077 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1076.924" y="0" xml:space="preserve">UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         </tspan></text>
+				</svg>
+				<svg width="2154" height="28" viewBox="0 0 2154 28">
+					<text id="UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_" data-name="UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         " transform="translate(1077 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1076.924" y="0" xml:space="preserve">UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         </tspan></text>
+				</svg>
+				<svg width="2154" height="28" viewBox="0 0 2154 28">
+					<text id="UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_" data-name="UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         " transform="translate(1077 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1076.924" y="0" xml:space="preserve">UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         </tspan></text>
+				</svg>
+				<svg width="2154" height="28" viewBox="0 0 2154 28">
+					<text id="UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_" data-name="UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         " transform="translate(1077 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1076.924" y="0" xml:space="preserve">UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         </tspan></text>
+				</svg>
+				<!-- repeated images below to keep up with animation -->
+				<svg width="2154" height="28" viewBox="0 0 2154 28">
+					<text id="UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_" data-name="UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         " transform="translate(1077 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1076.924" y="0" xml:space="preserve">UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         </tspan></text>
+				</svg>
+				<svg width="2154" height="28" viewBox="0 0 2154 28">
+					<text id="UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_" data-name="UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         " transform="translate(1077 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1076.924" y="0" xml:space="preserve">UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         </tspan></text>
+				</svg>
+				<svg width="2154" height="28" viewBox="0 0 2154 28">
+					<text id="UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_UNSERE_RÄUME_" data-name="UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         " transform="translate(1077 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1076.924" y="0" xml:space="preserve">UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         UNSERE RÄUME         </tspan></text>
+				</svg>
+			</div>
+		</div>
+
+		<!-- Intro Text -->
+
+		<div class="intro-text">
+			<h1><?php the_field('intro_text'); ?></h1>
+		</div>
+
+		<!-- Intro Text -->
+
+		<div class="ticker-tape-small">
+			<div class="inside">
+				<svg class="first" width="2244" height="28" viewBox="0 0 2244 28">
+					<text id="Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_" data-name="Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         " transform="translate(1122 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1121.055" y="0" xml:space="preserve">DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         </tspan></text>
+				</svg>
+				<svg width="2244" height="28" viewBox="0 0 2244 28">
+					<text id="Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_" data-name="Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         " transform="translate(1122 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1121.055" y="0" xml:space="preserve">DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         </tspan></text>
+				</svg>
+				<svg width="2244" height="28" viewBox="0 0 2244 28">
+					<text id="Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_" data-name="Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         " transform="translate(1122 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1121.055" y="0" xml:space="preserve">DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         </tspan></text>
+				</svg>
+				<svg width="2244" height="28" viewBox="0 0 2244 28">
+					<text id="Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_" data-name="Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         " transform="translate(1122 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1121.055" y="0" xml:space="preserve">DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         </tspan></text>
+				</svg>
+				<svg width="2244" height="28" viewBox="0 0 2244 28">
+					<text id="Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_" data-name="Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         " transform="translate(1122 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1121.055" y="0" xml:space="preserve">DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         </tspan></text>
+				</svg>
+				<svg width="2244" height="28" viewBox="0 0 2244 28">
+					<text id="Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_" data-name="Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         " transform="translate(1122 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1121.055" y="0" xml:space="preserve">DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         </tspan></text>
+				</svg>
+				<!-- repeated images below to keep up with animation -->
+				<svg width="2244" height="28" viewBox="0 0 2244 28">
+					<text id="Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_" data-name="Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         " transform="translate(1122 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1121.055" y="0" xml:space="preserve">DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         </tspan></text>
+				</svg>
+				<svg width="2244" height="28" viewBox="0 0 2244 28">
+					<text id="Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_" data-name="Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         " transform="translate(1122 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1121.055" y="0" xml:space="preserve">DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         </tspan></text>
+				</svg>
+				<svg width="2244" height="28" viewBox="0 0 2244 28">
+					<text id="Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_Das_Erdgeschoss_" data-name="Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         Das Erdgeschoss         " transform="translate(1122 22)" fill="#e6e6e6" font-size="20" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="-1121.055" y="0" xml:space="preserve">DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         DAS ERDGESCHOSS         </tspan></text>
+				</svg>
+			</div>
+		</div>
+
+		<!--Slider-->
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+		endwhile; // End of the loop.
+		?>
+
+		<!--Slider-->
+
+	</main><!-- #main -->
+
+
+<?php
+get_sidebar();
+get_footer();
