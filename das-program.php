139c8ed1a2d38e59a09376aebf9db13339fef28f
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-08 12:34:34 +0200

    added 2019

--- das-program.php
+++ das-program.php
@@ -202,7 +202,37 @@
 		</div>
 
 		<div id="2019" class="tabcontent">
-			<p>Events of 2019</p>
+			
+		<?php 
+			$args = array(
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'category_name' => '2019',
+				'posts_per_page' => 6,
+			);
+			$arr_posts = new WP_Query( $args );
+			 
+			if ( $arr_posts->have_posts() ) :
+				
+			 
+				while ( $arr_posts->have_posts() ) :
+					$arr_posts->the_post();
+					?>
+					<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<div class="event-header">
+							<p><?php the_field('type'); ?></p>
+							<a href="<?php the_permalink(); ?>"><h2><?php the_field('title'); ?></h2></a>
+							<p><?php the_field('when'); ?></p>
+						</div>
+						<div class="event-info">
+							<div class="details"><?php the_field('description'); ?></div>
+						</div>
+					</article>
+
+					<?php
+				endwhile;
+			endif; ?>
+
 		</div>
 
 	</div>
