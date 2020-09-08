1d7ab29b42cca7f35c946d2639e3da2867398ac3
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-08 12:33:35 +0200

    test

--- das-program.php
+++ das-program.php
@@ -218,7 +218,7 @@
 		for (i = 0; i < tablinks.length; i++) {
 			tablinks[i].className = tablinks[i].className.replace(" active", "");
 		}
-		document.getElementById(cityName).style.display = "block";
+		document.getElementById(cityName).style.display = "grid";
 		evt.currentTarget.className += " active";
 		}
 	</script>
