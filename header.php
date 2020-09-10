efd2711770846363768ca34b9dedbda1bff7cce7
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-10 11:55:37 +0200

    test

--- header.php
+++ header.php
@@ -188,6 +188,13 @@
 					array(
 						'theme_location' => 'menu-1',
 						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-2',
 						'menu_id'        => 'language-menu',
 					)
 				);
