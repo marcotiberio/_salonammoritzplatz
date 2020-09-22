bc8d050ef320ec53b378568d765fc46814bbd9a6
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-22 09:23:45 +0200

    update archive

--- das-program.php
+++ das-program.php
@@ -164,11 +164,16 @@
 		<div class="tab">
 			<button class="tablinks" onclick="openCity(event, '2020')">2020</button>
 			<button class="tablinks" onclick="openCity(event, '2019')">2019</button>
+			<button class="tablinks" onclick="openCity(event, '2018')">2018</button>
+			<button class="tablinks" onclick="openCity(event, '2017')">2017</button>
+			<button class="tablinks" onclick="openCity(event, '2016')">2016</button>
+			<button class="tablinks" onclick="openCity(event, '2015')">2015</button>
+			<button class="tablinks" onclick="openCity(event, '2014')">2014</button>
+			<button class="tablinks" onclick="openCity(event, '2013')">2013</button>
 		</div>
 
 		<!-- Tab content -->
 		<div id="2020" class="tabcontent" style="display: grid;">
-			
 			<?php 
 			$args = array(
 				'post_type' => 'post',
@@ -194,45 +199,234 @@
 							<div class="details"><?php the_field('description'); ?></div>
 						</div>
 					</article>
-
 					<?php
 				endwhile;
 			endif; ?>
 
 		</div>
-
+		<!-- Tab content -->
 		<div id="2019" class="tabcontent">
-			
-		<?php 
-			$args = array(
-				'post_type' => 'post',
-				'post_status' => 'publish',
-				'category_name' => '2019',
-				'posts_per_page' => 6,
-			);
-			$arr_posts = new WP_Query( $args );
-			 
-			if ( $arr_posts->have_posts() ) :
+			<?php 
+				$args = array(
+					'post_type' => 'post',
+					'post_status' => 'publish',
+					'category_name' => '2019',
+					'posts_per_page' => 6,
+				);
+				$arr_posts = new WP_Query( $args );
 				
-			 
-				while ( $arr_posts->have_posts() ) :
-					$arr_posts->the_post();
-					?>
-					<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<div class="event-header">
-							<p><?php the_field('type'); ?></p>
-							<a href="<?php the_permalink(); ?>"><h2><?php the_field('title'); ?></h2></a>
-							<p><?php the_field('when'); ?></p>
-						</div>
-						<div class="event-info">
-							<div class="details"><?php the_field('description'); ?></div>
-						</div>
-					</article>
+				if ( $arr_posts->have_posts() ) :
+					
+				
+					while ( $arr_posts->have_posts() ) :
+						$arr_posts->the_post();
+						?>
+						<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="event-header">
+								<p><?php the_field('type'); ?></p>
+								<a href="<?php the_permalink(); ?>"><h2><?php the_field('title'); ?></h2></a>
+								<p><?php the_field('when'); ?></p>
+							</div>
+							<div class="event-info">
+								<div class="details"><?php the_field('description'); ?></div>
+							</div>
+						</article>
 
-					<?php
-				endwhile;
-			endif; ?>
+						<?php
+					endwhile;
+				endif; ?>
+		</div>
+		<!-- Tab content -->
+		<div id="2018" class="tabcontent">
+			<?php 
+				$args = array(
+					'post_type' => 'post',
+					'post_status' => 'publish',
+					'category_name' => '2018',
+					'posts_per_page' => 6,
+				);
+				$arr_posts = new WP_Query( $args );
+				
+				if ( $arr_posts->have_posts() ) :
+					
+				
+					while ( $arr_posts->have_posts() ) :
+						$arr_posts->the_post();
+						?>
+						<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="event-header">
+								<p><?php the_field('type'); ?></p>
+								<a href="<?php the_permalink(); ?>"><h2><?php the_field('title'); ?></h2></a>
+								<p><?php the_field('when'); ?></p>
+							</div>
+							<div class="event-info">
+								<div class="details"><?php the_field('description'); ?></div>
+							</div>
+						</article>
 
+						<?php
+					endwhile;
+				endif; ?>
+		</div>
+		<!-- Tab content -->
+		<div id="2017" class="tabcontent">
+			<?php 
+				$args = array(
+					'post_type' => 'post',
+					'post_status' => 'publish',
+					'category_name' => '2017',
+					'posts_per_page' => 6,
+				);
+				$arr_posts = new WP_Query( $args );
+				
+				if ( $arr_posts->have_posts() ) :
+					
+				
+					while ( $arr_posts->have_posts() ) :
+						$arr_posts->the_post();
+						?>
+						<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="event-header">
+								<p><?php the_field('type'); ?></p>
+								<a href="<?php the_permalink(); ?>"><h2><?php the_field('title'); ?></h2></a>
+								<p><?php the_field('when'); ?></p>
+							</div>
+							<div class="event-info">
+								<div class="details"><?php the_field('description'); ?></div>
+							</div>
+						</article>
+
+						<?php
+					endwhile;
+				endif; ?>
+		</div>
+		<!-- Tab content -->
+		<div id="2016" class="tabcontent">
+			<?php 
+				$args = array(
+					'post_type' => 'post',
+					'post_status' => 'publish',
+					'category_name' => '2016',
+					'posts_per_page' => 6,
+				);
+				$arr_posts = new WP_Query( $args );
+				
+				if ( $arr_posts->have_posts() ) :
+					
+				
+					while ( $arr_posts->have_posts() ) :
+						$arr_posts->the_post();
+						?>
+						<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="event-header">
+								<p><?php the_field('type'); ?></p>
+								<a href="<?php the_permalink(); ?>"><h2><?php the_field('title'); ?></h2></a>
+								<p><?php the_field('when'); ?></p>
+							</div>
+							<div class="event-info">
+								<div class="details"><?php the_field('description'); ?></div>
+							</div>
+						</article>
+
+						<?php
+					endwhile;
+				endif; ?>
+		</div>
+		<!-- Tab content -->
+		<div id="2015" class="tabcontent">
+			<?php 
+				$args = array(
+					'post_type' => 'post',
+					'post_status' => 'publish',
+					'category_name' => '2015',
+					'posts_per_page' => 6,
+				);
+				$arr_posts = new WP_Query( $args );
+				
+				if ( $arr_posts->have_posts() ) :
+					
+				
+					while ( $arr_posts->have_posts() ) :
+						$arr_posts->the_post();
+						?>
+						<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="event-header">
+								<p><?php the_field('type'); ?></p>
+								<a href="<?php the_permalink(); ?>"><h2><?php the_field('title'); ?></h2></a>
+								<p><?php the_field('when'); ?></p>
+							</div>
+							<div class="event-info">
+								<div class="details"><?php the_field('description'); ?></div>
+							</div>
+						</article>
+
+						<?php
+					endwhile;
+				endif; ?>
+		</div>
+		<!-- Tab content -->
+		<div id="2014" class="tabcontent">
+			<?php 
+				$args = array(
+					'post_type' => 'post',
+					'post_status' => 'publish',
+					'category_name' => '2014',
+					'posts_per_page' => 6,
+				);
+				$arr_posts = new WP_Query( $args );
+				
+				if ( $arr_posts->have_posts() ) :
+					
+				
+					while ( $arr_posts->have_posts() ) :
+						$arr_posts->the_post();
+						?>
+						<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="event-header">
+								<p><?php the_field('type'); ?></p>
+								<a href="<?php the_permalink(); ?>"><h2><?php the_field('title'); ?></h2></a>
+								<p><?php the_field('when'); ?></p>
+							</div>
+							<div class="event-info">
+								<div class="details"><?php the_field('description'); ?></div>
+							</div>
+						</article>
+
+						<?php
+					endwhile;
+				endif; ?>
+		</div>
+		<!-- Tab content -->
+		<div id="2013" class="tabcontent">
+			<?php 
+				$args = array(
+					'post_type' => 'post',
+					'post_status' => 'publish',
+					'category_name' => '2013',
+					'posts_per_page' => 6,
+				);
+				$arr_posts = new WP_Query( $args );
+				
+				if ( $arr_posts->have_posts() ) :
+					
+				
+					while ( $arr_posts->have_posts() ) :
+						$arr_posts->the_post();
+						?>
+						<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="event-header">
+								<p><?php the_field('type'); ?></p>
+								<a href="<?php the_permalink(); ?>"><h2><?php the_field('title'); ?></h2></a>
+								<p><?php the_field('when'); ?></p>
+							</div>
+							<div class="event-info">
+								<div class="details"><?php the_field('description'); ?></div>
+							</div>
+						</article>
+
+						<?php
+					endwhile;
+				endif; ?>
 		</div>
 
 	</div>
