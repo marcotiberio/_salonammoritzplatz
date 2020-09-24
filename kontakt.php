7e2a0408462ced7ca6ffcbfa41240f3f8acbe4bc
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-24 09:31:09 +0200

    styling mailchimp

--- kontakt.php
+++ kontakt.php
@@ -62,14 +62,14 @@ get_header();
 			<!-- Begin Mailchimp Signup Form -->
 			<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
 			<style type="text/css">
-				#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+				#mc_embed_signup{background:#FFFEF8; clear:left; font: 1.2rem 'vremena book'; }
 				/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
 				We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 			</style>
 			<div id="mc_embed_signup">
 			<form action="https://salonammoritzplatz.us10.list-manage.com/subscribe/post?u=c02cb311ffcc0e45aa9232489&amp;id=7f055f7b32" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 				<div id="mc_embed_signup_scroll">
-				<label for="mce-EMAIL"><h2>Newsletter</h2></label>
+				<label for="mce-EMAIL">Newsletter</label>
 				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
 				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 				<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c02cb311ffcc0e45aa9232489_7f055f7b32" tabindex="-1" value=""></div>
