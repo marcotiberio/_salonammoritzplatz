b7a74bfe2260b1cf69f1d93311e83a2e69e2c864
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-24 09:52:42 +0200

    revert prev form

--- kontakt.php
+++ kontakt.php
@@ -60,27 +60,18 @@ get_header();
 			<?php endif; ?>
 
 			<!-- Begin Mailchimp Signup Form -->
-			<p>
-				<label>Email address: 
-					<input type="email" name="EMAIL" placeholder="Your email address" required />
-			</label>
-			</p>
-
-			<p>
-				<input type="submit" value="Sign up" />
-			</p>
-			<!-- Begin Mailchimp Signup Form -->
-			<!-- <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+			<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
 			<div id="mc_embed_signup">
 			<form action="https://salonammoritzplatz.us10.list-manage.com/subscribe/post?u=c02cb311ffcc0e45aa9232489&amp;id=7f055f7b32" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 				<div id="mc_embed_signup_scroll">
 				<label for="mce-EMAIL">Newsletter</label>
 				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 				<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c02cb311ffcc0e45aa9232489_7f055f7b32" tabindex="-1" value=""></div>
 				<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 				</div>
 			</form>
-			</div> -->
+			</div>
 			<!--End mc_embed_signup-->
 
 		</div>
