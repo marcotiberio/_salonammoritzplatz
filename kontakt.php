d2091c2bcb7174c5df8a7180a3dc303921b0dca0
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-24 09:35:45 +0200

    removed inline style mailchimp

--- kontakt.php
+++ kontakt.php
@@ -61,11 +61,6 @@ get_header();
 
 			<!-- Begin Mailchimp Signup Form -->
 			<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
-			<style type="text/css">
-				#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-				/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-				We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-			</style>
 			<div id="mc_embed_signup">
 			<form action="https://salonammoritzplatz.us10.list-manage.com/subscribe/post?u=c02cb311ffcc0e45aa9232489&amp;id=7f055f7b32" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 				<div id="mc_embed_signup_scroll">
